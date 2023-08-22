3e19236db84c1514b7df569577ac2f9b328ace9b
Sanoj Ahamed <sanojahamed1730@gmail.com>
2023-08-22 23:19:38 +0530

    API Intergrate and Final Update

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatbtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 
-let userMessage;
-const API_KEY = "";
 
-
+let userMessage = null; // Variable to store user's message
+const API_KEY = ""; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
     //Create a chat <li> element with passed message and class name 
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", className);
     let chatContent = className === "outgoing"
-    ? `<p>${message}</p>`
-    : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+    ? `<p></p>`
+    : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
-    return chatLi;
-
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
 }
 
 const generateResponse = (incomingChatli) => {
@@ -40,14 +42,12 @@ const generateResponse = (incomingChatli) => {
             ]
         })
     };
+
 // Send POST request to API, get response and set the response as paragraph text
-fetch(API_URL, requestOptions)
-.then(res => res.json())
-.then(data => {
+fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
     messageElement.textContent = data.choices[0].message.content.trim();
 })
-.catch(() => {
-    messageElement.classList.add("error");
+.catch(() => {messageElement.classList.add("error");
     messageElement.textContent = "Oops! Something went wrong. Please try again.";
 })
 .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
@@ -55,20 +55,41 @@ fetch(API_URL, requestOptions)
  
 
 const handleChat = () => {
-    userMessage = chatInput.value.trim();
+    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
     if (!userMessage) return;
+
+    // Clear the input textarea and set its height to default
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
 
     // Append the user's message to the chatbox
     const outgoingChatli = createChatLi(userMessage, "outgoing");
     chatbox.appendChild(outgoingChatli);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
 
     setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatli = createChatLi("Thinking...", "incoming");
+        // Display "Typing..." message while waiting for the response
+        const incomingChatli = createChatLi("Typing...", "incoming");
         chatbox.appendChild(incomingChatli);
         generateResponse(incomingChatli);
     }, 600);
 };
 
-sendChatbtn.addEventListener("click", handleChat);
+chatInput.addEventListener("input", () => {
+    // Adjust the height of the input textarea based on its content
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
 
+chatInput.addEventListener("keydown", (e) => {
+    // If Enter key is pressed without Shift key and the window 
+    // width is greater than 800px, handle the chat
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
+
+sendChatbtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
